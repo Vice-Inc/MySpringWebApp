d7ed021088771fb8f6a39b3da409da751b410235
Vice-Inc <k-d-a-200@mail.ru>
2021-03-15 20:47:12 +0300

    Добавлена страница редактирования пользователей
    Пользователей может редачить только админ

--- src/main/java/org/vice/spring/controller/UserController.java
+++ src/main/java/org/vice/spring/controller/UserController.java
@@ -1,14 +1,23 @@
 package org.vice.spring.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.vice.spring.domain.Role;
 import org.vice.spring.repository.UserRepository;
+import org.vice.spring.domain.User;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/user")
+@PreAuthorize("hasAuthority('ADMIN')")
 public class UserController {
 
     @Autowired
@@ -16,9 +25,40 @@ public class UserController {
 
     @GetMapping
     public String userList(Model model){
-
         model.addAttribute("users", userRepository.findAll());
-
         return "userList";
     }
+
+    @GetMapping("{user}")
+    public String userEditForm(@PathVariable User user,
+                               Model model){
+        model.addAttribute("user", user);
+        model.addAttribute("roles", Role.values());
+        return "userEdit";
+    }
+
+    @PostMapping
+    public String userSave(@RequestParam(name = "userId") User user,
+                           @RequestParam String username,
+                           @RequestParam Map<String, String> form){
+
+        user.setUsername(username);
+
+        Set<String> roleSet = Arrays.stream(Role.values())
+                .map(Role::name)
+                .collect(Collectors.toSet());
+
+        user.getRoles().clear();
+
+        for(String key : form.keySet()){
+            if(roleSet.contains(key)){
+                user.getRoles().add(Role.valueOf(key));
+            }
+        }
+
+        userRepository.save(user);
+
+        return "redirect:/user";
+    }
+
 }
