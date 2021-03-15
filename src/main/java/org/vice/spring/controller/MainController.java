2553a226e3570f374bdfe19610c35d119b0262cc
Vice-Inc <k-d-a-200@mail.ru>
2021-03-15 21:51:53 +0300

    Теперь можно загружать изображения
    Пользователь может прикреплять изображения к сообщению

--- src/main/java/org/vice/spring/controller/MainController.java
+++ src/main/java/org/vice/spring/controller/MainController.java
@@ -1,9 +1,11 @@
 package org.vice.spring.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.multipart.MultipartFile;
 import org.vice.spring.domain.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,13 +13,19 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.vice.spring.domain.Message;
 import org.vice.spring.repository.MessageRepository;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Map;
+import java.util.UUID;
 
 @Controller
 public class MainController {
 
     @Autowired
     private MessageRepository messageRepository;
+
+    @Value("${upload.path}")
+    private String uploadPath;
 
     @GetMapping("/")
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name,
@@ -47,8 +55,25 @@ public class MainController {
     public String add(@AuthenticationPrincipal User user,
                       @RequestParam String text,
                       @RequestParam String tag,
-                      Map<String, Object> model){
+                      @RequestParam("file") MultipartFile file,
+                      Map<String, Object> model) throws IOException {
         Message message = new Message(text, tag, user);
+
+        if(file!=null){
+            File uploadDir = new File(uploadPath);
+            if(!uploadDir.exists()){
+                uploadDir.mkdir();
+            }
+
+            String uuidFile = UUID.randomUUID().toString();
+            String resultFilename =
+                    uuidFile + "." + file.getOriginalFilename();
+
+            file.transferTo(new File(uploadPath + "/" + resultFilename));
+
+            message.setFilename(resultFilename);
+        }
+
         messageRepository.save(message);
         model.put("messages", messageRepository.findAll());
         return "main";
--- src/main/java/org/vice/spring/domain/Message.java
+++ src/main/java/org/vice/spring/domain/Message.java
@@ -17,6 +17,8 @@ public class Message {
     //множество сообщений к одному пользователю
     private User author;
 
+    private String filename;
+
     public Message() {
     }
 
@@ -65,5 +67,13 @@ public class Message {
 
     public void setAuthor(User author) {
         this.author = author;
+    }
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public void setFilename(String filename) {
+        this.filename = filename;
     }
 }
